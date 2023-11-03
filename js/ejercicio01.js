0bc3890a4937823ca89bf93c9a0c252fe94c8237
Florencia Gonzalez <iturbe.mariaflorencia@gmail.com>
2023-11-03 16:25:42 -0300

    agregue el ejercicio 1 de la card del 25/10

---
+++ js/ejercicio01.js
@@ -0,0 +1,11 @@
+let num1 = 10, num2 = 2;
+
+let suma = num1 + num2;
+let resta = num1 - num2;
+let multiplicacion = num1 * num2;
+let division = num1 / num2;
+
+console.log('Suma: ' + suma);
+console.log('Resta: ' + resta);
+console.log('Multiplicacion: ' + multiplicacion);
+console.log('Division: ' + division);
