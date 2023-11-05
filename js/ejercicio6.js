1cae8f02c244bbd95b1367f551c80f3b0ba59293
Florencia Gonzalez <iturbe.mariaflorencia@gmail.com>
2023-11-05 17:19:23 -0300

    agregue el ejercicio 6

---
+++ js/ejercicio6.js
@@ -0,0 +1,8 @@
+let numero1 = parseInt(prompt('Ingrese un número'));
+let numero2 = parseInt(prompt('Ingrese un número'));
+
+if (numero1 < numero2) {
+    console.log('El '+ numero2 + ' es el número más grande');
+} else {
+    console.log('El '+ numero1 + ' es el número más grande');
+}
