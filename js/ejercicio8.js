40f65dd0ef4f7328ce89b0f1dfdc545801bc4f7e
Florencia Gonzalez <iturbe.mariaflorencia@gmail.com>
2023-11-05 17:26:47 -0300

    agregue el ejercicio 8

---
+++ js/ejercicio8.js
@@ -0,0 +1,7 @@
+let numero = parseInt(prompt("Ingrese un número"));
+
+if (numero % 2 == 0) {
+    alert('El '+ numero + ' es divisible en 2')
+} else {
+    alert('El '+ numero + ' no es divisible en 2')
+}
