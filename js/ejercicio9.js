6002fbf09f359e167098fed0e21454dc27cb6b48
Florencia Gonzalez <iturbe.mariaflorencia@gmail.com>
2023-11-05 22:20:46 -0300

    agregue el triple igual en la condicion logica del if

--- js/ejercicio9.js
+++ js/ejercicio9.js
@@ -9,11 +9,11 @@ console.log(frase.charAt(0));
 
 for (let i = 0; i < frase.length; i++) {
   if (
-    frase.charAt(i) == "a" ||
-    frase.charAt(i) == "e" ||
-    frase.charAt(i) == "i" ||
-    frase.charAt(i) == "o" ||
-    frase.charAt(i) == "u"
+    frase.charAt(i) === "a" ||
+    frase.charAt(i) === "e" ||
+    frase.charAt(i) === "i" ||
+    frase.charAt(i) === "o" ||
+    frase.charAt(i) === "u"
   ) {
     document.write(frase.charAt(i));
   }
